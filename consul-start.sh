c377cb5b1b87ca3f1a3a7b28db6274d305a8d4e1
James Powis <powisj@gmail.com>
2017-11-05 17:59:33 -0700

    fix default setting

--- consul-start.sh
+++ consul-start.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # Set target environment variables for advertisement
-my_int=$1 || "en0" # en0 is the default nic on a mac, sorry linux users, patch welcomed
+my_int=$1
+if [ ! $my_int ]; then
+  my_int="en0" # en0 is the default nic on a mac, sorry linux users, patches welcome
+fi
+
 my_ip=`ifconfig $my_int | grep "inet " | awk '{print $2}'`
 
 # Start Consul
